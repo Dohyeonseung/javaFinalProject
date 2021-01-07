40d07e202c2a565c05a04a98542d8c2a3ee61adc
ygm23 <ygm23@DESKTOP-T6S7HCT>
2021-01-07 23:30:19 +0900

    prouduct

---
+++ finalproject/src/main/java/com/sp/app/Product.java
@@ -0,0 +1,47 @@
+package com.sp.app;
+
+public class Product {
+	private int productNum;
+	private String productName;
+	private int price;
+	private String imageFilename;
+
+
+
+
+	public int getProductNum() {
+		return productNum;
+	}
+
+	public void setProductNum(int productNum) {
+		this.productNum = productNum;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	
+	public String getImageFilename() {
+		return imageFilename;
+	}
+
+	public void setImageFilename(String imageFilename) {
+		this.imageFilename = imageFilename;
+	}
+
+	
+	
+}
